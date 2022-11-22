e799a4a8e3db576bee9b0cfd6bf3ef1f77f89b10
yevil rojas <yevilrojas93@hotmail.com>
2022-11-22 12:54:41 -0300

    nuevos cambios v4

--- php/registraragencia.php
+++ php/registraragencia.php
@@ -12,10 +12,7 @@
 			$sql->bindParam(":agencia_id",$_POST["agencia_id"],PDO::PARAM_INT);	
 		}
 		$checkAgencia = $sql->fetchAll();
-		var_dump($checkAgencia);
-		var_dump($_POST["agencia_id"]);
-		var_dump($_POST["idAgencia"]);
-		var_dump($_POST);
+
 		if (count($checkAgencia) > 0) {
 			$error .= "_repeated";
 		}
@@ -54,15 +51,15 @@
 			$sql->bindParam(":localidad",$_POST["localidad"],PDO::PARAM_STR);
 			$sql->bindParam(":telefono",$_POST["telefono"],PDO::PARAM_STR);
 			if ($sql->execute()) {
-				//header("location:/home/".$url);
+				header("location:/home/".$url);
 			}
 		}
 		else{
 			if (isset($_POST['edit']) AND $_POST["edit"] == true) {
-				//header("location:/editaragencia/".$_POST["old_id"]."/".$error);
+				header("location:/editaragencia/".$_POST["old_id"]."/".$error);
 			}
 			else{
-				//header("location:/registraragencia/".$error);
+				header("location:/registraragencia/".$error);
 			}
 		}
 	}
