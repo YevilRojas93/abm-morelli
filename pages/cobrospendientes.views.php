e9cd5625c5d1b17e8180d8ca724da72d83a5238f
emanuel <emanueldavidroca@gmail.com>
2022-11-29 18:57:09 -0300

    cambios

--- pages/cobrospendientes.views.php
+++ pages/cobrospendientes.views.php
@@ -1,3 +1,5 @@
+	<?php error_reporting(E_ALL);
+ini_set("display_errors", 1); ?>
 	<section id="section-listado" class="container">
 		<h1 class="bg-success text-center px-0 mb-0">COBROS PENDIENTES</h1>
 			<form method="post" class="row py-2 px-3 text-center">
@@ -41,7 +43,7 @@
 			  			$agencia = "AND a.idUsuario = ".$_SESSION['idUsuario'];
 			  		}
 
-					$sql = Conexion::conectar()->prepare("SELECT c.monto,c.idCobro,a.nombre_agencia as agencia,c.tipo_pago,c.fecha_cobro,c.status FROM cobros as c INNER JOIN agencias as a ON a.idAgencia = c.idAgencia WHERE  cobros.fecha_cobro BETWEEN :fecha_inicio and :fecha_fin $agencia AND c.status = 'pendiente'");
+					$sql = Conexion::conectar()->prepare("SELECT c.monto,c.idCobro,a.nombre_agencia as agencia,c.tipo_pago,c.fecha_cobro,c.status FROM cobros as c INNER JOIN agencias as a ON a.idAgencia = c.idAgencia WHERE c.fecha_cobro BETWEEN :fecha_inicio and :fecha_fin $agencia AND c.status = 'pendiente'");
 					$sql->bindParam(":fecha_inicio",$fecha_inicio,PDO::PARAM_STR);
 					$sql->bindParam(":fecha_fin",$fecha_fin,PDO::PARAM_STR);
 					$sql->execute();
