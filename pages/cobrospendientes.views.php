6eaf35213e6d667ce6daeeffadfc909083aa6b4d
emanuel <emanueldavidroca@gmail.com>
2022-11-29 18:39:46 -0300

    cambios

--- pages/cobrospendientes.views.php
+++ pages/cobrospendientes.views.php
@@ -1,5 +1,19 @@
 	<section id="section-listado" class="container">
 		<h1 class="bg-success text-center px-0 mb-0">COBROS PENDIENTES</h1>
+			<form method="post" class="row py-2 px-3 text-center">
+				<div class="col-md-5">
+					<label for="" class="fw-bold">Fecha inicio</label>
+					<input type="text" name="fecha_inicio" id="fecha_inicio" class="form-control">
+				</div>
+				<div class="col-md-5">
+					<label for="" class="fw-bold">Fecha fin</label>
+					<input type="text" name="fecha_fin" id="fecha_fin" class="form-control">
+				</div>
+				<div class="col-md-2">
+					<label for=""></label>
+					<button class="btn btn-success w-100" type="submit">Filtrar</button>
+				</div>
+			</form>
 			<table class="table border border-4 ">
 			  <thead>
 			    <tr>
@@ -15,13 +29,22 @@
 			  	<?php 
 					require_once("php/conexion.php"); 
 			  		$dia_actual = date("Y-m-d");
+				  	$fecha_inicio = $_POST["fecha_inicio"] ?? date('Y-m-d',);  
+				  	$date = new DateTime(date("Y-m-d")); // Y-m-d
+					$date->add(new DateInterval('P90D'));
+					$fin_default =  $date->format('Y-m-d');
+				  	$fecha_fin = $_POST["fecha_fin"] ?? $fin_default;  
+
+				  	$fecha_inicio = htmlentities($fecha_inicio,ENT_COMPAT,"UTF-8");
+				  	$fecha_fin = htmlentities($fecha_fin,ENT_COMPAT,"UTF-8");
 			  		$agencia = "";
 			  		if ($_SESSION["tipo"] == "agenciero") {
 			  			$agencia = "AND a.idUsuario = ".$_SESSION['idUsuario'];
 			  		}
 
-					$sql = Conexion::conectar()->prepare("SELECT c.monto,c.idCobro,a.nombre_agencia as agencia,c.tipo_pago,c.fecha_cobro,c.status FROM cobros as c INNER JOIN agencias as a ON a.idAgencia = c.idAgencia WHERE fecha_cobro >= :dia_actual $agencia AND c.status = 'pendiente'");
-					$sql->bindParam(":dia_actual",$dia_actual,PDO::PARAM_STR);
+					$sql = Conexion::conectar()->prepare("SELECT c.monto,c.idCobro,a.nombre_agencia as agencia,c.tipo_pago,c.fecha_cobro,c.status FROM cobros as c INNER JOIN agencias as a ON a.idAgencia = c.idAgencia WHERE  cobros.fecha_cobro BETWEEN :fecha_inicio and :fecha_fin $agencia AND c.status = 'pendiente'");
+					$sql->bindParam(":fecha_inicio",$fecha_inicio,PDO::PARAM_STR);
+					$sql->bindParam(":fecha_fin",$fecha_fin,PDO::PARAM_STR);
 					$sql->execute();
 					$lista_cobros = $sql->fetchAll();
 					if (count($lista_cobros) == 0) {
